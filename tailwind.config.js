83c0f0ae9fcfe5890c916bca2c95d31c21c81cd9
nedhi116 <nadhiijemal503@gmail.com>
2026-08-02 00:32:02 +0300

    Add explanatory comment to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // Scan source files so unused Tailwind classes get purged
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
