06c76fef95cc078d68a7df096587b9da80687ee9
nedhi116 <nadhiijemal503@gmail.com>
2026-08-02 00:49:34 +0300

    Add explanatory comment to ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
+// ESLint flat config applying recommended and React-specific rules
 export default tseslint.config(
   { ignores: ['dist'] },
   {
