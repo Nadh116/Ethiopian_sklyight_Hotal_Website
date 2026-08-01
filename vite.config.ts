4cfd91787e13cadc82bc6f098c44072031eae552
nedhi116 <nadhiijemal503@gmail.com>
2026-08-02 00:31:47 +0300

    Add explanatory comment to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// Vite configuration enabling React plugin for the project
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
