8a35c5c54637b3757937f50f500819ab667f995b
nedhi116 <nadhiijemal503@gmail.com>
2026-08-02 00:23:28 +0300

    Add explanatory comment to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Mount the React application into the root DOM element
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
