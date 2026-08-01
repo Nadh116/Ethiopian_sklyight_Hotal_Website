3cac2a48b3cc8509fc6291e38dddca354fb0ccfd
nedhi116 <nadhiijemal503@gmail.com>
2026-08-02 00:50:47 +0300

    Add explanatory comment to WelcomeSection stars

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -31,6 +31,7 @@ const WelcomeSection = () => {
                 <div className="text-sm text-gray-600">Service</div>
               </div>
             </div>
+            {/* Five gold stars representing guest satisfaction */}
             <div className="flex items-center space-x-1 mb-4">
               {[...Array(5)].map((_, i) => (
                 <Star key={i} className="h-5 w-5 text-yellow-400 fill-current" />
